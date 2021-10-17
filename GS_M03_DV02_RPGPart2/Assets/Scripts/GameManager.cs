3fc6b9be32c0bec9c266a4a82eaeb05c172e4455
Garrett Smith <g.smith61900@gmail.com>
2021-10-17 13:35:47 -0700

    init commit

---
+++ GS_M03_DV02_RPGPart2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPun
+{
+    [Header("Players")]
+    public PlayerController[] players;
+    public string playerPrefabPath;
+    public Transform[] spawnPoints;
+    public float respawnTime;
+
+    private int playersInGame;
+
+    public static GameManager instance;
+
+    
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        players = new PlayerController[PhotonNetwork.PlayerList.Length];
+
+        photonView.RPC("ImInGame", RpcTarget.AllBuffered);
+    }
+    [PunRPC]
+    void ImInGame()
+    {
+        playersInGame++;
+
+        if (playersInGame == PhotonNetwork.PlayerList.Length)
+            SpawnPlayer();
+  
+    }
+
+    [PunRPC]
+    void SpawnPlayer()
+    {
+        GameObject playerObj = PhotonNetwork.Instantiate(playerPrefabPath, spawnPoints[Random.Range(0, spawnPoints.Length)].position, Quaternion.identity);
+
+        playerObj.GetComponent<PhotonView>().RPC("Initialize", RpcTarget.All, PhotonNetwork.LocalPlayer);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GS_M03_DV02_RPGPart2
+
+Link to playable: https://itsgarbear.github.io/GS_M03_DV02_RPGPart2/
  
